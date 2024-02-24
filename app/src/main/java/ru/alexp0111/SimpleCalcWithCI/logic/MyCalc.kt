08156114818dd3b8a3bb81862b1e91117c67b6ba
Alexander <alexp0111@mail.ru>
2024-02-24 23:37:28 +0300

    try to broke tests

--- app/src/main/java/ru/alexp0111/SimpleCalcWithCI/logic/MyCalc.kt
+++ app/src/main/java/ru/alexp0111/SimpleCalcWithCI/logic/MyCalc.kt
@@ -2,6 +2,6 @@ package ru.alexp0111.SimpleCalcWithCI.logic
 
 class MyCalc {
     fun findSum(first: Double, second: Double): Double {
-        return first + second
+        return first + second + 1.0
     }
 }
