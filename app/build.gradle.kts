d9a9a99bad4e7468eebf9f0f57bcf26aa8deab41
Alexander <alexp0111@mail.ru>
2024-02-27 21:37:26 +0300

    Add autoTests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,19 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Junit
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    // Espresso
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    // Robolectric environment
+    testImplementation ("org.robolectric:robolectric:4.4")
+    // Mockito framework
+    testImplementation ("org.mockito:mockito-core:5.1.1")
 }
---
+++ app/src/androidTest/java/ru/alexp0111/SimpleCalcWithCI/MainActivityTest.kt
@@ -0,0 +1,42 @@
+package ru.alexp0111.SimpleCalcWithCI
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @JvmField
+    @Rule
+    val mActivityRule = ActivityTestRule(
+        MainActivity::class.java,
+        true,
+        false,
+    )
+
+    @Before
+    fun setUp() {
+        // Before actions
+    }
+
+    @Test
+    fun shouldSumToValues() {
+        mActivityRule.launchActivity(null)
+        onView(withId(R.id.et_first_value)).perform(typeText("59"))
+        onView(withId(R.id.et_second_value)).perform(typeText("9"))
+        onView(withId(R.id.btn_find_sum)).perform(click())
+        onView(withId(R.id.txt_result)).check(matches(withText("69.0")))
+    }
+}
